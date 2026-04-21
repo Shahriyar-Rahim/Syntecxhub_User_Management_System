4ac9cbb0b58470e62bd9f3d7de986ff426e36512
Shahriyar-Rahim <rahimislam420.ri35@gmail.com>
2026-04-22 00:05:09 +0600

    created databse connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
+import connectDB from './config/db.config.js';
 // import dns from 'dns'
 
 // dns.setServers(['8.8.8.8', '1.1.1.1'])
@@ -14,6 +15,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+connectDB();
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
